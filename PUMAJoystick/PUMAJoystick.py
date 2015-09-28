02061a59eb44549311828683cefb6f363646582e
newyinzer <newyinzer66@gmail.com>
2015-09-28 01:34:13 +0000

    fixed axes

--- PUMAJoystick/PUMAJoystick.py
+++ PUMAJoystick/PUMAJoystick.py
@@ -14,18 +14,20 @@ j.init()
 print 'Initialized Joystick : %s' % j.get_name()
 
 # Keeps a history of axes
-axis_history = [0,0,0,0,0]
-axis_val = 0
+axis_l = 0 #1
+axis_r = 0 #3
+axis_val = 0.0
 
 try:
     while True:
         pygame.event.pump()
-	print '%i %i' % (axis_history[2],axis_history[3])
-        for i in range(0, j.get_numaxes()):
-		axis_val = j.get_axis(i)
-		axis_val = axis_val * 32767.0
-		axis_history[i] = int(axis_val)
-		#print '%f' % axis_val
-		#axis_history[i] = j.get_axis(i)
+	axis_val = j.get_axis(1)
+	axis_val = axis_val * -32767.0
+	axis_l = int(axis_val)
+	axis_val = j.get_axis(3)
+	axis_val = axis_val * -32767.0
+	axis_r = int(axis_val)
+	print '%i %i' % (axis_l,axis_r)
+	time.sleep(0.1) # one sheep, two sheep, it's time to sleep
 except KeyboardInterrupt:
     j.quit()
