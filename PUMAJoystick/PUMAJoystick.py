63e6724510b59ad81b440799f08165ec13d75464
newyinzer <newyinzer66@gmail.com>
2015-09-27 20:27:17 -0400

    Update PUMAJoystick.py

--- PUMAJoystick/PUMAJoystick.py
+++ PUMAJoystick/PUMAJoystick.py
@@ -1,28 +1,14 @@
 '''
-basic_control.py
-@author: Garrett Owen
-@date: January 26, 2012
+PUMAJoystick.py
 
 Based off code from 
 http://iamtherockstar.com/archive/making-hid-devices-easier-using-pygame-joysticks/
 '''
 
-import serial
 import pygame
 import time
 
-#serialPort = '/dev/tty.usbserial-A7004Jg4' # Arduino Mega
-serialPort = '/dev/tty.usbmodemfd121'       # Arduino Uno
-baudRate = 9600
-
-# Open Serial Connection to Arduino Board
-ser = serial.Serial(serialPort, baudRate, timeout=1);
-
-'''
-Gets joystick data and prints it
-'''
 pygame.init()
-#joystick.init()
 j = pygame.joystick.Joystick(0)
 j.init()
 print 'Initialized Joystick : %s' % j.get_name()
@@ -35,19 +21,11 @@ try:
     while True:
         pygame.event.pump()
 
-#        # Used to read input from the two joysticks       
-#        for i in range(0, j.get_numaxes()):
-#            if j.get_axis(i) != 0.00:
-#                print 'Axis %i reads %.2f' % (i, j.get_axis(i))
-#        
-#        time.sleep(1)
-
         for i in range(0, j.get_numbuttons()):
             if j.get_button(i) != 0:
                 if not button_history[i]:
                     print 'Button %i reads %i' % (i, j.get_button(i))
                     button_history[i] = 1
-                    ser.write(str(i))
             else:
                 button_history[i] = 0
 
