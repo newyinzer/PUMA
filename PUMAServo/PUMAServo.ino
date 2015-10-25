366881e8864bde2e4c620dc124e80f596e0829cd
newyinzer <newyinzer66@gmail.com>
2015-10-25 02:46:47 +0000

    servo stuff

--- PUMAServo/PUMAServo.ino
+++ PUMAServo/PUMAServo.ino
@@ -6,7 +6,7 @@ Servo servo1;
 void setup() {
 
   pinMode(1,OUTPUT);
-  servo1.attach(14); //analog pin 0
+  servo1.attach(9); //analog pin 0
   Serial.begin(19200);
   Serial.println("Ready");
 
@@ -15,7 +15,7 @@ void setup() {
 void loop() {
 
   static int v = 0;
-
+/*
   if ( Serial.available()) {
     char ch = Serial.read();
 
@@ -29,7 +29,29 @@ void loop() {
         break;
     }
   }
-
-  Servo::refresh();
-
+*/
+  //servo1.write(0);
+  //delay(1000);
+  /*
+  servo1.write(10);
+  delay(1000);
+  servo1.write(20);
+  delay(1000);
+  servo1.write(30);
+  delay(1000);
+  servo1.write(40);
+  delay(1000);
+  servo1.write(50);
+  delay(1000);
+  servo1.write(60);
+  delay(1000);
+  servo1.write(70);
+  delay(1000);
+  servo1.write(80);
+  delay(1000);
+  */
+  servo1.write(20);
+  delay(3000);
+  servo1.write(110);
+  delay(3000);
 }
