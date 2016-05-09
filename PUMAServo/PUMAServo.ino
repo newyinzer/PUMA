6e1b86e7eb96cd0bb7f1fcbdbf2f8660971eb8f0
newyinzer <newyinzer66@gmail.com>
2016-05-09 01:28:48 +0000

    got turret rotation back up

--- PUMAServo/PUMAServo.ino
+++ PUMAServo/PUMAServo.ino
@@ -15,49 +15,12 @@ void setup() {
 }
 
 void loop() {
-
-  static int v = 0;
-/*
-  if ( Serial.available()) {
-    char ch = Serial.read();
-
-    switch(ch) {
-      case '0'...'9':
-        v = v * 10 + ch - '0';
-        break;
-      case 's':
-        servo1.write(v);
-        v = 0;
-        break;
-    }
-  }
-*/
-  //servo1.write(0);
-  //delay(1000);
-  /*
-  servo1.write(10);
-  delay(1000);
-  servo1.write(20);
-  delay(1000);
-  servo1.write(30);
-  delay(1000);
-  servo1.write(40);
-  delay(1000);
-  servo1.write(50);
-  delay(1000);
-  servo1.write(60);
-  delay(1000);
-  servo1.write(70);
-  delay(1000);
-  servo1.write(80);
-  delay(1000);
-  */
+  // CCW
+  servo1.write(92);
+  delay(10000);
   
-  servo1.write(30);
-  delay(5000);
-  servo1.write(150);
-  delay(5000);
+  // CW
+  servo1.write(97);
+  delay(10000);
   
-  //servo1.write(100);
-  //delay(1000);
 }
