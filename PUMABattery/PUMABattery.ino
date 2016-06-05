07ba4a4aa3392b68ce727a37304e8eb9db51126e
newyinzer <newyinzer66@gmail.com>
2016-06-05 17:42:24 -0400

    changing conversion constants and delay

--- PUMABattery/PUMABattery.ino
+++ PUMABattery/PUMABattery.ino
@@ -1,10 +1,11 @@
 // for battery test
 
-#define delay1 1000
+#define delay1 5000
 
-unsigned long curtime, lastime;
+unsigned long curtime, lastime, sectime;
 
 int analogPin, voltageLevel;
+float voltage;
 
 void setup() {
   Serial.begin(19200);
@@ -13,15 +14,19 @@ void setup() {
   lastime = curtime;
   analogPin = 0;
   voltageLevel = 0;
+  voltage = 0.0;
+  sectime = 0;
 }
 
 void loop() {
   curtime = millis();
   voltageLevel = analogRead(analogPin);
+  voltage = (voltageLevel * (5.0 / 1024.0)) * 2.0;
+  sectime = curtime / 1000;
   if(curtime >= (lastime + delay1)) {
-    Serial.print(curtime);
+    Serial.print(sectime);
     Serial.print("\t");
-    Serial.print(voltageLevel);
+    Serial.print(voltage);
     Serial.print("\n");
     lastime = curtime;
   }
