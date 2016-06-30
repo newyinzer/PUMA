d321657dd77e2a732d5660707dc7d916a4cbeab8
newyinzer <newyinzer66@gmail.com>
2016-06-30 10:56:00 -0400

    Update PUMALog2.py

--- PUMALog/PUMALog2.py
+++ PUMALog/PUMALog2.py
@@ -1,9 +1,19 @@
 from openxc.interface import UsbVehicleInterface
 
-def receive(message, **kwargs):
+def receive(measurement, **kwargs):
     # this callback will receive each message received as a dict
-    print(message['name'])
-
+    if measurement.name == 'steering_wheel_angle':
+        print datetime.now()
+        print ',steering_wheel_angle,'
+        print measurement.value
+        print '\n'
+    elif measurement.name == 'vehicle_speed':
+        print datetime.now()
+        print ',vehicle_speed,'
+        print vehicle_speed
+        print '\n'
+        
+    
 vi = UsbVehicleInterface(callback=receive)
 vi.start()
 # This will block until the connection dies or you exit the program
