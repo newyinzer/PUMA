0123f87f1472d17351c6ef784589eab311533259
newyinzer <newyinzer66@gmail.com>
2016-10-12 09:39:12 -0400

    Complete Setup Stage

--- PUMAPanel/PUMAPanel.ino
+++ PUMAPanel/PUMAPanel.ino
@@ -1,38 +1,46 @@
 // For Switch Control
 
-int SPINS[] = {22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52};
-int SPIND[] = {23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53};
+// Analog In 0-13 on 54-67
+// H-Bridge Control on 22-53
+
+// State Enumeration
+static const int S = 0; // Straight
+static const int SD = 1; // Straight to Divering
+static const int D = 2; // Diverging
+static const int DS = 3; // Diverging to Straight
+static const int divergingThreshold = 512; // HIGH is diverging, LOW is straight
+
+int SPINS[] = {22,24,26,28,30,32,34,36,38,40, 42, 44, 46, 48, 50, 52}; // Straight Track Pins
+int SPIND[] = {23,25,27,29,31,33,35,37,39,41, 43, 45, 47, 49, 51, 53}; // Diverging Track Pins
+int SPINA[] = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15}; // Switch Input Pins
+int SPINB[] = {SD,SD,SD,SD,SD,SD,SD,SD,SD,SD, SD, SD, SD, SD, SD, SD}; // Switch States
 int maxPins = 16;
 
-int PIN00D = 23;
-int PIN01S = 24;
-int PIN01D = 25;
-int PIN02S = 26;
-int PIN02D = 27;
-
 void setup() {
-  // Analog In 0-13 on 54-67
-  // H-Bridge Control on 22-53
+  // Start Serial Communication
+  Serial.begin(19200);
+  Serial.println("Starting Panel");
   
-  // Set Up Output Pins
+  // Set Up H-Bridge Control Pins
   int i = 0;
+  int switchInput = 0;
   for(i = 0; i < maxPins; i++) {
     pinMode(SPINS[i], OUTPUT);
     pinMode(SPIND[i], OUTPUT);
   }
-  pinMode(PIN00S, INPUT);
-  pinMode(PIN00D, INPUT);
-  pinMode(PIN01S, INPUT);
-  pinMode(PIN01D, INPUT);
-  pinMode(PIN02S, INPUT);
-  pinMode(PIN02D, INPUT);
+  for(i = 0; i < maxPins; i++) {
+    digitalWrite(SPINS[i], LOW);
+    digitalWrite(SPIND[i], LOW);
+  }
+  Serial.println("Control Pin Setup Complete");
   
-  pinMode(1,OUTPUT);
-  servo1.attach(9); //analog pin 0
-  
-  Serial.begin(19200);
-  Serial.println("Ready");
-  
+  // Read Initial States
+  for(i = 0; i < maxPins; i++) {
+    switchInput = analogRead(SPINA[i]);
+    if(switchInput > divergingThreshold) { SPINB[i] = SD; }
+    else { SPINB[i] = DS; }
+  }
+  Serial.println("Current Switch States Read");
 }
 
 void loop() {
