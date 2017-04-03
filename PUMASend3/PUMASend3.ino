682c1be19d790c38eb8f406b4dc5176f5e9ab39d
newyinzer <newyinzer66@gmail.com>
2017-04-03 15:53:02 -0400

    Update PUMASend3.ino

--- PUMASend3/PUMASend3.ino
+++ PUMASend3/PUMASend3.ino
@@ -1,5 +1,5 @@
 /* 
-PUMASend2.ino
+PUMASend3.ino
 
 Description:
 Control module sends two joystick values to the PUMA.
@@ -31,7 +31,6 @@ Turret Data:
 #include <RF24.h>
 #include <RF24_config.h>
 #include <nRF24L01.h>
-//#include <printf.h>
 
 // Pin Assignments
 #define ERR_PIN 3 // Turn on if message not sent
@@ -115,50 +114,22 @@ byte processTurret() {
   return output;
 }
 
-/*
-// Original processJoystick function, changing due to new standard
-byte processJoystick(int val) {
-  byte output = 0;
-  int outint = val;
-  if(outint >= 512) {
-    // forward
-    outint = outint - 512;
-    outint = outint >> 2;
-  }
-  else if (outint <= 480) {
-    // reverse
-    outint = outint >> 2;
-    outint = outint + 128;
-  }
-  else {
-    // stop
-    outint = 0;
-  }
-  
-  output = (byte) outint;
-  return output;
-}
-*/
-
 // setup: Sets up system
 void setup()
 {
   // Start serial transmission
-  //Serial.begin(9600);
+  Serial.begin(9600);
   
-  // Wait 2 seconds
-  delay(2000);
-  //printf_begin();
+  // Wait 1 second
+  delay(1000);
   
   // Start Radio
   done = radio.begin();
-  radio.enableDynamicPayloads();
-  //radio.setRetries(5,15);
-  radio.setRetries(5,0);
+  //radio.setChannel(108);  // Above most Wifi Channels <-- do we need this?
+  radio.setPALevel(RF24_PA_MAX);
   radio.openWritingPipe(pipe_t);
-  radio.openReadingPipe(1,pipe_r);
-  radio.startListening();
-  
+  delay(1000);
+
   // Initialize variables
   lval = 0;
   rval = 0;
@@ -238,59 +209,6 @@ void loop()
   fval++;
 }
 
-/* YourDuinoStarter Example: Simple nRF24L01 Transmit
-  - WHAT IT DOES: Transmits simple fixed data with nRF24L01 radio
-  - SEE the comments after "//" on each line below
-   Start with radios about 4 feet apart.
-  - SEE the comments after "//" on each line below
-  - CONNECTIONS: nRF24L01 Modules See:
-  http://arduino-info.wikispaces.com/Nrf24L01-2.4GHz-HowTo
-  Uses the RF24 Library by TMRH2o here:
-  https://github.com/TMRh20/RF24
-   1 - GND
-   2 - VCC 3.3V !!! NOT 5V
-   3 - CE to Arduino pin 7
-   4 - CSN to Arduino pin 8
-   5 - SCK to Arduino pin 13
-   6 - MOSI to Arduino pin 11
-   7 - MISO to Arduino pin 12
-   8 - UNUSED
-
-   V1.02 02/06/2016
-   Questions: terry@yourduino.com */
-
-/*-----( Import needed libraries )-----*/
-#include <SPI.h>   // Comes with Arduino IDE
-#include "RF24.h"  // Download and Install (See above)
-/*-----( Declare Constants and Pin Numbers )-----*/
-//None yet
-/*-----( Declare objects )-----*/
-// (Create an instance of a radio, specifying the CE and CS pins. )
-RF24 myRadio (7, 8); // "myRadio" is the identifier you will use in following methods
-/*-----( Declare Variables )-----*/
-byte addresses[][6] = {"1Node"}; // Create address for 1 pipe.
-int dataTransmitted;  // Data that will be Transmitted from the transmitter
-
-void setup()   /****** SETUP: RUNS ONCE ******/
-{
-  // Use the serial Monitor (Symbol on far right). Set speed to 115200 (Bottom Right)
-  Serial.begin(115200);
-  delay(1000);
-  Serial.println(F("RF24/Simple Transmit data Test"));
-  Serial.println(F("Questions: terry@yourduino.com"));
-  dataTransmitted = 100; // Arbitrary known data to transmit. Change it to test...
-  myRadio.begin();  // Start up the physical nRF24L01 Radio
-  myRadio.setChannel(108);  // Above most Wifi Channels
-  // Set the PA Level low to prevent power supply related issues since this is a
-  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
-  myRadio.setPALevel(RF24_PA_MIN);
-  //  myRadio.setPALevel(RF24_PA_MAX);  // Uncomment for more power
-
-  myRadio.openWritingPipe( addresses[0]); // Use the first entry in array 'addresses' (Only 1 right now)
-  delay(1000);
-}//--(end setup )---
-
-
 void loop()   /****** LOOP: RUNS CONSTANTLY ******/
 {
   myRadio.write( &dataTransmitted, sizeof(dataTransmitted) ); //  Transmit the data
@@ -303,7 +221,4 @@ void loop()   /****** LOOP: RUNS CONSTANTLY ******/
 
 }//--(end main loop )---
 
-/*-----( Declare User-written Functions )-----*/
 
-
-//*********( THE END )***********
